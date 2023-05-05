895bc5d4a3237f1f308e867faeb0c975e5fec287
mcappucci34 <m.cappucci34@gmail.com>
2023-05-05 15:19:28 -0400

    Add start page and play button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,21 @@ import "../styles/App.css";
 
 export function App() {
 	return (
-		<div id="background">
-			<h1 className="display-1 text-white text-center">Asteroids</h1>
+		<div
+			id="background"
+			className="d-flex text-white justify-content-center align-items-center"
+		>
+			<div className="">
+				<h1 className="display-1">Asteroids</h1>
+				<div className="d-flex justify-content-center">
+					<button
+						id="play-game"
+						className="mx-auto bg-black text-white test"
+					>
+						<h2 className="animate-underline">Play Game</h2>
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
