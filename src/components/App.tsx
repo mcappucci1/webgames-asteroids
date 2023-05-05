d58f424fc233a69c0f69df4d72b6c0da9cdfae39
mcappucci34 <m.cappucci34@gmail.com>
2023-05-05 10:02:43 -0400

    Initial creation of game

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import "../styles/App.css";
+
+export function App() {
+	return (
+		<div id="background">
+			<h1 className="display-1 text-white text-center">Asteroids</h1>
+		</div>
+	);
+}
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<App />
+);
